18837e8a105f3c3ebea1d3c54f7ee08f25ecffea
vexelb <vexelb@gmail.com>
2019-02-26 10:55:08 +0300

    i hope it'll finally work

--- gameclient.py
+++ gameclient.py
@@ -155,7 +155,7 @@ def init():
     interface = Interface()
     bullets = []
     run = True
-    sock.connect(('176.59.9.55', 9090))
+    sock.connect(('172.20.10.13', 9090))
     while run:
         def reciever():
             data1 = sock.recv(512).decode()
