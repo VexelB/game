7d0235cc0b7e4f509274a823ad32b36bb914d60c
vexelb <vexelb@gmail.com>
2019-02-27 14:56:15 +0300

    still problem with score drawing, actual death and near restart added

--- gameclient.py
+++ gameclient.py
@@ -75,7 +75,7 @@ def init():
 
     bullets = []
     run = True
-    sock.connect(('localhost', 9090))
+    sock.connect(('192.168.43.174', 9090))
     while run:
         def reciever():
             data1 = sock.recv(512).decode()
